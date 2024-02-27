36b8491fbd0ea4459b4ec8b8a6497def9ff884a2
pierre-adrien@marque <pierre-adrien.marque@institutsolacroup.com>
2024-02-27 15:57:23 +0100

    navBar

--- client/src/pages/navBar.jsx
+++ client/src/pages/navBar.jsx
@@ -1,5 +1,51 @@
+import React, { useState, useEffect, useRef } from 'react';
 import style from './navBar.module.css';
 
 export const NavBar=()=>{
 
+    // Etat pour gérer l'affichage des menu
+    const [menuVisible, setMenuVisible]=useState(false);
+    const menuRef = useRef();
+
+    // Fonction pour basculer la visibilité du menu
+    const toggleMenu = () => {
+        setMenuVisible(!menuVisible);
+    };
+
+    // Fonction pour fermer le menu lorsque l'utilisateur clique à l'extérieur de celui-ci
+    const handleClickOutside = (event) => {
+        if (menuRef.current && !menuRef.current.contains(event.target)) {
+            setMenuVisible(false);
+        }
+    };
+    
+    useEffect(() => {
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
+    return(
+        <nav className={style.nav}>
+            <img className={style.img} src='./public/logo.png' alt='log_chez_gustave'/>
+            
+            <div class={style.bouton}>
+                <button class={style.connect}>Se Connecter</button>
+                <button class={style.recherche}>Rechercher</button>            
+            </div>
+
+            <div className={style.menu} onClick={toggleMenu} ref={menuRef}>
+                <div className={style.menuBurgerIcon}>Menu</div>
+            </div>
+
+            {menuVisible && (
+                <div className={style.menuList}>
+                    <a href='#'>Se connecter</a>
+                    <a href='#'>Rechercher</a>
+                </div>
+            )}
+
+        </nav>
+    )
 }
