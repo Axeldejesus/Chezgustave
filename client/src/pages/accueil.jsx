030ec12b075aa56d37f6f4bdae3068234b38293f
pierre-adrien@marque <pierre-adrien.marque@institutsolacroup.com>
2024-02-28 15:56:16 +0100

    formulaire

--- client/src/pages/accueil.jsx
+++ client/src/pages/accueil.jsx
@@ -2,6 +2,41 @@ import style from './accueil.module.css';
 
 export const Accueil=()=>{
     return(
-        <h1>Bienvenue chez Gustave</h1>
+        <>
+            <h1>Bienvenue chez Gustave</h1>
+            <form className={style.from} action="" method="get">
+                <div className={style.champ}>
+                    <label for="localisation">Où allez vous?</label>
+                    <input type="text" value="localisation"/>
+                </div>
+
+                <div className={style.champ}>
+                    <label for="dateDepart">Départ</label>
+                    <input type="date" value="dateDepart"/>
+                </div>
+
+                <div className={style.champ}>
+                    <label for="dateArrive">Arrivé</label>
+                    <input type="date" value="dateArrive"/>
+                </div>
+
+                <div className={style.champ}>
+                    <label for="nbAdulte">Adulte</label>
+                    <input type="number" value="nbAdulte" min="0" max="10"/>
+                </div>
+
+                <div className={style.champ}>
+                    <label for="nbEnfants">Enfants</label>
+                    <input type="number" value="nbEnfants" min="0" max="10"/>
+                </div>
+
+                <div className={style.champ}>
+                    <label for="nbChambre">Chambre</label>
+                    <input type="number" value="nbChambre" min="0" max="10"/>
+                </div>
+
+                <input className={style.inputButton} type="submit" value="Rechercher"/>
+            </form>
+        </>
     )
 }
