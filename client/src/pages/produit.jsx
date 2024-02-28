fea4c6af2b6fdaab26b2fecd5e7a367d5702d55a
pieguezou <pieguezou@gmail.com>
2024-02-28 09:20:47 +0100

    commit

--- client/src/pages/produit.jsx
+++ client/src/pages/produit.jsx
@@ -1,5 +1,14 @@
+import React from 'react';
 import style from './produit.module.css';
 
-export const Produit=()=>{
-
-}
+export const Produit = () => {
+  return (
+    <header className={style.header}>
+      <h1>Bienvenue sur notre site de produits</h1>
+      <nav>
+       <img src='./public/logo.png' alt='log_chez_gustave'></img>
+       <div className='bouttou'></div>
+      </nav>
+    </header>
+  );
+};
