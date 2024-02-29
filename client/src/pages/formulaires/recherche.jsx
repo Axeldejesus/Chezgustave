9bb288c45fd13df85a6c364221530a59db277601
pierre-adrien@marque <pierre-adrien.marque@institutsolacroup.com>
2024-02-29 10:12:21 +0100

    recherche formulaire

---
+++ client/src/pages/formulaires/recherche.jsx
@@ -0,0 +1,40 @@
+import style from './recherche.module.css';
+
+export const Recherche=()=>{
+
+    return(
+        <form className={style.from} action="" method="get">
+            <div className={style.champ}>
+                <label for="localisation">Où allez vous?</label>
+                <input type="text" value="localisation"/>
+            </div>
+
+            <div className={style.champ}>
+                <label for="dateDepart">Départ</label>
+                <input type="date" value="dateDepart"/>
+            </div>
+
+            <div className={style.champ}>
+                <label for="dateArrive">Arrivé</label>
+                <input type="date" value="dateArrive"/>
+            </div>
+
+            <div className={style.champ}>
+                <label for="nbAdulte">Adulte</label>
+                <input type="number" value="nbAdulte" min="0" max="10"/>
+            </div>
+
+            <div className={style.champ}>
+                <label for="nbEnfants">Enfants</label>
+                <input type="number" value="nbEnfants" min="0" max="10"/>
+            </div>
+
+            <div className={style.champ}>
+                <label for="nbChambre">Chambre</label>
+                <input type="number" value="nbChambre" min="0" max="10"/>
+            </div>
+
+            <input className={style.inputButton} type="submit" value="Rechercher"/>
+        </form>
+    )
+}
