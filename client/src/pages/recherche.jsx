5ed1a728510c66f39cf24a99209b2eb72aedd1f9
pierre-adrien@marque <pierre-adrien.marque@institutsolacroup.com>
2024-03-04 11:06:32 +0100

    pathmane footer

--- client/src/pages/recherche.jsx
+++ client/src/pages/recherche.jsx
@@ -1,5 +1,16 @@
+import { FormulaireRecherche } from './formulaires/formulaireRecherche';
 import style from './recherche.module.css';
 
 export const Recherche=()=>{
 
+    return(
+        <>
+            <FormulaireRecherche/>
+            <section className={style.main}>
+                <section className={style.critere}>
+                   <h2>Prix</h2> 
+                </section>
+            </section>
+        </>
+    )
 }
