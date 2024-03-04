a09d268d69fd25a6e3b5415aa1234b02ab8d5db4
pieguezou <pieguezou@gmail.com>
2024-03-04 12:11:19 +0100

    commit

--- client/src/pages/profil.jsx
+++ client/src/pages/profil.jsx
@@ -19,8 +19,19 @@ export const Profil = () => {
                             <li className={style.submenuItem}>Teste
                                 <ul className={style.subSubmenu}>
                                     <li><a href="#" className={style.subSubmenuLink} onClick={toggleFormVisibility}>Mes informations</a></li>
+                                    <br/>
+                                    <br/>
+                                    <br/>
                                     <li><a href="#" className={style.subSubmenuLink}>Mes commandes</a></li>
+                                    <br/>
+                                    <br/>
+                                    <br/>
                                     <li><a href="#" className={style.subSubmenuLink}>Mes réclamations</a></li>
+                                    <br/>
+                                    <br/>
+                                    <br/>
+                                    <li><a href="#" className={style.subSubmenuLink}>Mes réclamations</a></li>
+
                                 </ul>
                             </li>
                         </ul>
@@ -28,24 +39,44 @@ export const Profil = () => {
                 </ul>
             </div>
 
-            {/* Formulaire affiché en fonction de l'état showForm */}
             {showForm && (
                 <div className={style.overlay}>
                     <div className={style.formContainer}>
                         <h2>Mes informations</h2>
-                        <form>
+                        <div className={style.formRow}>
                             <label className={style.label} htmlFor="nom">Nom :</label>
-                            <input className={style.input} type="text" id="nom" name="nom" />
+                            <input className={style.input} type="text" id="nom" name="nom" defaultValue="Pourvoir" />
+                            <br/>
+                            <br/>
+                        </div>
+                        <div className={style.formRow}>
                             <label className={style.label} htmlFor="prenom">Prénom :</label>
-                            <input className={style.input} type="text" id="prenom" name="prenom" />
+                            <input className={style.input} type="text" id="prenom" name="prenom" defaultValue="Test" />
+                            <br/>
+                            <br/>
+                        </div>
+                        <div className={style.formRow}>
                             <label className={style.label} htmlFor="email">Email :</label>
-                            <input className={style.input} type="email" id="email" name="email" />
+                            <input className={style.input} type="email" id="email" name="email" defaultValue="testpourvoir@gmail.com" />
+                            <br/>
+                            <br/>
+                        </div>
+                        <h2>Changement de mot de passe </h2>
+                        <div className={style.formRow}>
                             <label className={style.label} htmlFor="password">Mot de passe actuel :</label>
                             <input className={style.input} type="password" id="password" name="password" />
+                            <br/>
+                            <br/>
+                        </div>
+                        <div className={style.formRow}>
                             <label className={style.label} htmlFor="newPassword">Nouveau mot de passe :</label>
                             <input className={style.input} type="password" id="newPassword" name="newPassword" />
+                            <br/>
+                            <br/>
+                        </div>
+                        <div className={style.formRow}>
                             <input className={style.submitButton} type="submit" value="Soumettre" />
-                        </form>
+                        </div>
                     </div>
                 </div>
             )}
