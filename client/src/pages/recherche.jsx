dd4de3b40aa53a4eb15395fdf0d8ae2ad52690c9
pierre-adrien@marque <pierre-adrien.marque@institutsolacroup.com>
2024-03-06 10:23:06 +0100

    recherche

--- client/src/pages/recherche.jsx
+++ client/src/pages/recherche.jsx
@@ -1,15 +1,42 @@
 import { FormulaireRecherche } from './formulaires/formulaireRecherche';
 import style from './recherche.module.css';
 
-export const Recherche=()=>{
-
-    return(
+export const Recherche = () => {
+    return (
         <>
-            <FormulaireRecherche/>
+            <section className={style.recherche}>
+                <FormulaireRecherche />
+            </section>
+
             <section className={style.main}>
-                <section className={style.critere}>
-                   <h2>Prix</h2> 
-                </section>
+                <div className={style.critere}>
+                    <h2>Prix</h2>
+                    <h2>Équipements</h2>
+                    <p>equipement 1</p>
+                    <p>equipement 2</p>
+                    <p>equipement 3</p>
+                    <p>equipement 4</p>
+                    <h2>Catégories</h2>
+                    <p>categorie 1</p>
+                    <p>categorie 2</p>
+                    <p>categorie 3</p>
+                    <p>categorie 4</p>
+                </div>
+
+                <div className={style.resultat}>
+                    <div className={style.contenueResult}>
+                        <div className={style.leftResult}/>
+                        <div className={style.rightResult}/>
+                    </div>
+                    <div className={style.contenueResult}>
+                        <div className={style.leftResult}/>
+                        <div className={style.rightResult}/>
+                    </div>
+                    <div className={style.contenueResult}>
+                        <div className={style.leftResult}/>
+                        <div className={style.rightResult}/>
+                    </div>
+                </div>
             </section>
         </>
     )
