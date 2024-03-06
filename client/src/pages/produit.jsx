a9c9f6a09e8c6fc2dc96bd6be2bc710ce8929fbc
pieguezou <pieguezou@gmail.com>
2024-03-06 11:20:05 +0100

    commit

--- client/src/pages/produit.jsx
+++ client/src/pages/produit.jsx
@@ -6,12 +6,16 @@ export const Produit = () => {
     <div className={style.produitContainer}>
       <div className={style.texteEtBouton}>
         <div className={style.texte}>
-          <h1>Belle maison sur la côte Saint-Malo, Bretagne france </h1>
-          <textarea placeholder="Ceci est une superbe zone de texte pour descriptifs de l'annonce et tout."></textarea>
+          <h1>Belle maison sur la côte Saint-Malo, Bretagne, France</h1>
+          <br/>
+          <br/>
+          <h4>Ceci est une superbe zone de texte pour descriptifs de l'annonce et tout.</h4>
         </div>
-        <button className="button">Réserver un séjour</button>
+        {/* Le bouton est stylisé selon les classes CSS */}
+        <button className={style.button}>Réserver un séjour</button>
       </div>
       <div className={style.imageContainer}>
+        {/* Placeholder pour l'image, ajustez comme nécessaire */}
         <div className={style.placeholder}></div>
       </div>
     </div>
